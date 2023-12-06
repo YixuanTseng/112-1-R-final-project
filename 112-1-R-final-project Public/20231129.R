a557a8d5eb5c7046d1c0a6b100e7221076c1f152
YixuanTseng <144878782+YixuanTseng@users.noreply.github.com>
2023-12-06 14:43:36 +0800

    update

--- 112-1-R-final-project Public/20231129.R
+++ 112-1-R-final-project Public/20231129.R
@@ -6,4 +6,28 @@ dplyr::glimpse(dataSet)
 dataSet$區 |> 
   table() |>
   sort(decreasing = TRUE)
-#
+
+# 最常發生事故的位置
+dataSet$事故位置 |>
+  table() |>
+  sort(decreasing = T)
+
+# 當事者區分
+dataSet$`當事者區分(類別)` |>
+  table() |>
+  sort(decreasing = T)
+
+#主要肇因
+dataSet$肇事因素主要 |>
+  table() |>
+  sort(decreasing = T)
+
+#飲酒情形
+dataSet$飲酒情形 |>
+  table() |>
+  sort(decreasing = T)
+
+#事故類型及型態
+dataSet$事故類型及型態 |>
+  table() |>
+  sort(decreasing = T)
